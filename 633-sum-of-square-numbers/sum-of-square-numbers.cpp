f503091f5b86e28c2ef4175b86701152ea8b86a9
ankit-ydv25 <ankitydv25052007@gmail.com>
2026-08-22 09:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (22.93%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+    int i=0,j=sqrt(c);
+        while(i<=j)
+        {
+            if((pow(i,2)+pow(j,2) )== c)
+            {
+                return true;
+            }    
+            else if((pow(i,2)+pow(j,2))<c)
+            {
+                i++;
+            }    
+            else j--;
+        }
+        return false;
+    }
+};
