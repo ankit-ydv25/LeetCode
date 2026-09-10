845ef083797605ce6d1cda0ec1db810a28805d3a
ankit-ydv25 <ankitydv25052007@gmail.com>
2026-09-10 18:26:52 +0530

    Time: 0 ms (100%), Space: 13.7 MB (79.39%) - LeetHub-Neo

---
+++ ankit-ydv25/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int neg=0;
+        for(int value : nums)
+        {
+            if(value==0)
+            {
+                return 0;
+            }
+            if(value<0)
+            {
+                neg++;
+            }
+        }
+        if(neg%2==0)
+        {
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+};
