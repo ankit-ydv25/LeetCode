2fe7ac392362bff4e3a3c41a215f01ebb455c8f3
ankit-ydv25 <ankitydv25052007@gmail.com>
2026-09-22 15:08:31 +0530

    Time: 3 ms (11.96%), Space: 7.9 MB (32.24%) - LeetHub-Neo

---
+++ ankit-ydv25/2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        if(n==1)
+        {
+            return 1;
+        }
+        long long sum=1;
+        for(int i=2;i<=n;i++)
+        {
+            sum = sum + 4*(i-1);
+        }
+        return sum;
+    }
+};
