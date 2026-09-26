4fea8172faf46d343682deed2c4399212fe8b902
ankit-ydv25 <ankitydv25052007@gmail.com>
2026-09-26 20:21:09 +0530

    Time: 4 ms (65.59%), Space: 13.5 MB (19.4%) - LeetHub-Neo

---
+++ ankit-ydv25/1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int i=0;
+        int j=n;
+        vector<int> ans;
+        while(i!=n)
+        {
+            ans.push_back(nums[i]);
+            i++;
+            ans.push_back(nums[j]);
+            j++;
+        }
+        return ans;
+    }
+};
